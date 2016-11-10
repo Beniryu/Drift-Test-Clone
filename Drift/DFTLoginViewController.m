2f36f7ccf745f6b94d9eea50e05d47077eaa5fbb
Thierry Ng <ng.thierry.d@gmail.com>
2016-11-10 22:06:32 +0100

    Adding Pods + renaming assets + WIP Interface

--- Drift/DFTLoginViewController.m
+++ Drift/DFTLoginViewController.m
@@ -10,6 +10,17 @@
 
 @interface DFTLoginViewController ()
 
+#pragma mark Alternative login buttons
+
+@property (weak, nonatomic) IBOutlet UIButton *facebookButton;
+@property (weak, nonatomic) IBOutlet UIButton *googleButton;
+@property (weak, nonatomic) IBOutlet UIButton *numberButton;
+@property (weak, nonatomic) IBOutlet UIButton *fingerprintButton;
+
+#pragma mark Text Fields
+@property (weak, nonatomic) IBOutlet UITextField *emailTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+
 @end
 
 @implementation DFTLoginViewController
@@ -18,9 +29,19 @@
 {
 	[super viewDidLoad];
 
-
+	[self configureViews];
 }
 
+- (void)configureViews
+{
+	[self.facebookButton setImage:[UIImage imageNamed:@"facebook_icon"] forState:UIControlStateNormal];
+	[self.googleButton setImage:[UIImage imageNamed:@"google_icon"] forState:UIControlStateNormal];
 
+
+	self.facebookButton.tintColor = [UIColor colorWithRed:0.60 green:0.69 blue:0.79 alpha:1.0];;
+	self.googleButton.tintColor = [UIColor colorWithRed:0.60 green:0.69 blue:0.79 alpha:1.0];;
+	self.emailTextField.userInteractionEnabled = NO;
+	self.passwordTextField.userInteractionEnabled = NO;
+}
 
 @end
--- Podfile
+++ Podfile
@@ -1,9 +1,12 @@
-# Uncomment this line to define a global platform for your project
-# platform :ios, '6.0'
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '9.0'
 
 target 'Drift' do
+  # Uncomment the next line if you're using Swift or would like to use dynamic frameworks
+  # use_frameworks!
 
-pod 'AFNetworking'
+  # Pods for Drift
+
+  pod 'AFNetworking', '~> 3.0'
 
 end
-
