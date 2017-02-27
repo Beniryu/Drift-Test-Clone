81deec98817af2bd3980e98b57534c2a48bf92ef
Thierry Ng <ng.thierry.d@gmail.com>
2017-02-27 21:48:18 +0100

    Drift CollectionView

--- Drift/DFTDriftViewController.m
+++ Drift/DFTDriftViewController.m
@@ -6,21 +6,55 @@
 //  Copyright © 2017 Thierry Ng. All rights reserved.
 //
 
-#import "DriftViewController.h"
+#import "DFTDriftViewController.h"
 
-@interface DFTDriftViewController ()
+@interface DFTDriftViewController () <UICollectionViewDelegate, UICollectionViewDataSource>
+
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
 
 @end
 
-@implementation DriftViewController
+@implementation DFTDriftViewController
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 
-
+	[self configureCollectionView];
 }
 
+- (void)configureCollectionView
+{
+	self.collectionView.delegate = self;
+	self.collectionView.dataSource = self;
 
+	UICollectionViewFlowLayout *layout = [UICollectionViewFlowLayout new];
+
+	layout.itemSize = (CGSize){self.collectionView.frame.size.width - 32, 167};
+	layout.sectionInset = UIEdgeInsetsMake(0, 16, 0, 16);
+	layout.minimumLineSpacing = 16.;
+	layout.scrollDirection = UICollectionViewScrollDirectionHorizontal;
+
+	self.collectionView.collectionViewLayout = layout;
+	self.collectionView.showsHorizontalScrollIndicator = NO;
+	self.collectionView.pagingEnabled = YES;
+}
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
+{
+	return (1);
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+	return (5);
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+	UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"Test" forIndexPath:indexPath];
+
+	return (cell);
+}
 
 @end
