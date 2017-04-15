f2ee7101fcd65fcde393347f03ac3c692cd90c4c
Clément Georgel <clement.georgel@arkeup.com>
2017-04-15 23:20:08 +0200

    Ajout des éléments et des tests divers sur les ajouts de tags Déplacement des éléments du bloc 1 au 2 lors du passage.

--- Drift/DFTAddDropViewController.m
+++ Drift/DFTAddDropViewController.m
@@ -13,10 +13,14 @@
 
 #import <AMTagListView.h>
 
-@interface DFTAddDropViewController () <UIScrollViewDelegate, UITextFieldDelegate, AMTagListDelegate>
+@interface DFTAddDropViewController () <UIScrollViewDelegate, UITextFieldDelegate, AMTagListDelegate, UITextViewDelegate>
 {
 @private
-    int nbTags;
+    UIControl *activeField;
+    CGPoint savedContentOffset;
+    NSArray *uiElementFirstBlock;
+    BOOL keyboardActivated;
+    NSArray *stepOneDisable;
 }
 
 #pragma mark - Outlets -
@@ -30,6 +34,7 @@
 @property (weak, nonatomic) IBOutlet UIView *vLocation;
 @property (weak, nonatomic) IBOutlet UILabel *lblLocation;
 @property (weak, nonatomic) IBOutlet UITextView *titleTextView;
+@property (weak, nonatomic) IBOutlet UILabel *lblSeparator;
 @property (weak, nonatomic) IBOutlet UITextField *tfTags;
 @property (weak, nonatomic) IBOutlet AMTagListView *tagsView;
 @property (weak, nonatomic) IBOutlet UIButton *btnTags;
@@ -72,6 +77,10 @@ static const int MAX_CARACTERS_AUTHORIZED   = 8;
 {
 	[super viewDidLoad];
 
+    uiElementFirstBlock = @[self.lblStep, self.lblStepNumber, self.vLocation, self.titleTextView, self.lblSeparator, self.tagsView, self.btnTags, self.tfDescription, self.btnDescription];
+    
+    stepOneDisable = @[self.btnTags, self.btnDescription, self.tfTags, self.tfDescription, self.titleTextView];
+    
 	UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(didPan:)];
 
 	[self.view addGestureRecognizer:pan];
@@ -79,6 +88,7 @@ static const int MAX_CARACTERS_AUTHORIZED   = 8;
 	self.titleTextView.scrollEnabled = NO;
 	[self configureTags];
 	[self configureTableView];
+    [self registerForKeyboardNotifications];
 }
 
 - (void)viewDidAppear:(BOOL)animated
@@ -104,8 +114,9 @@ static const int MAX_CARACTERS_AUTHORIZED   = 8;
 
 	self.tfTags.delegate = self;
 	self.tagsView.tagListDelegate = self;
-
-	self.tfTags.hidden = NO;
+    self.titleTextView.delegate = self;
+    
+    self.tfTags.hidden = YES;
 	[self.tagsView addGestureRecognizer:tap];
 	[self.tagsView setTapHandler:^(AMTagView *tag) {
 		[self removeTag:tag];
@@ -150,11 +161,13 @@ static const int MAX_CARACTERS_AUTHORIZED   = 8;
 
 - (void)showTagsTextField
 {
-	self.tfTags.hidden = NO;
+//	self.tfTags.hidden = NO;
 }
 
 - (void)executeTransition:(kDFTDropFormStepTransition)transition
 {
+    if( keyboardActivated )
+        return;
 	switch (transition)
 	{
 		case kDFTDropFormStepTransitionDetailsToSettings:
@@ -185,11 +198,17 @@ static const int MAX_CARACTERS_AUTHORIZED   = 8;
 		 self.titleTextView.transform = t;
 
 		 CGAffineTransform t2 = CGAffineTransformTranslate(CGAffineTransformIdentity, 0, -(self.titleHeight * 0.5));
-		 self.tagsView.transform = t2;
-		 self.tfDescription.transform = t2;
+         self.tagsView.transform = t2;
+         self.tfDescription.transform = t2;
+         self.lblSeparator.transform = t2;
+         self.btnTags.transform = t2;
+         self.btnDescription.transform = t2;
 
 		 [self.scrollView setContentOffset:(CGPoint){0, 80} animated:YES];
 	 }];
+    
+    for( UIView *element in stepOneDisable )
+        element.userInteractionEnabled = NO;
 }
 
 - (void)transitionFromSettingsToValidation
@@ -207,11 +226,17 @@ static const int MAX_CARACTERS_AUTHORIZED   = 8;
 	[UIView animateWithDuration:0.5 animations:
 	 ^{
 		 self.titleTextView.transform = CGAffineTransformIdentity;
-		 self.tagsView.transform = CGAffineTransformIdentity;
-		 self.tfDescription.transform = CGAffineTransformIdentity;
+         self.tagsView.transform = CGAffineTransformIdentity;
+         self.tfDescription.transform = CGAffineTransformIdentity;
+         self.lblSeparator.transform = CGAffineTransformIdentity;
+         self.btnTags.transform = CGAffineTransformIdentity;
+         self.btnDescription.transform = CGAffineTransformIdentity;
 
 		 [self.scrollView setContentOffset:(CGPoint){0, 0} animated:YES];
 	 }];
+    
+    for( UIView *element in stepOneDisable )
+        element.userInteractionEnabled = YES;
 }
 
 #pragma mark
@@ -219,16 +244,13 @@ static const int MAX_CARACTERS_AUTHORIZED   = 8;
 
 - (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string
 {
-//	if ([string isEqualToString:@" "])
-//	{
-//		if (textField.text.length > 0)
-//		{
-//			[self.tagsView addTag:textField.text];
-//			textField.text = @"";
-//		}
-//        
-//		return NO;
-//	}
+	if ([string isEqualToString:@" "])
+	{
+		if (textField.text.length > 0)
+			[self addTag:textField.text];
+        
+		return NO;
+	}
     if( textField.text.length + string.length > MAX_CARACTERS_AUTHORIZED )
         return NO;
 	return YES;
@@ -240,25 +262,44 @@ static const int MAX_CARACTERS_AUTHORIZED   = 8;
     return YES;
 }
 
+- (void)textFieldDidBeginEditing:(UITextField *)textField
+{
+    activeField = textField;
+}
+
+- (void)textFieldDidEndEditing:(UITextField *)textField
+{
+    activeField = nil;
+}
+
 #pragma mark - Gestion Tags
 
 -(void) addTag:(NSString *) tag
 {
-    NSString *tagStrim = [tag stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
-    if( nbTags < MAX_TAG_AUTHORIZED && tag.length > 0)
+    NSString *tagTrim = [tag stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+    if( self.tagsView.tags.count < MAX_TAG_AUTHORIZED && tagTrim && tagTrim.length > 0)
     {
-        [self.tagsView addTag:tagStrim];
+        BOOL found = NO;
+        for( UIView<AMTag> *tag in self.tagsView.tags )
+        {
+            if( [tag.tagText caseInsensitiveCompare:tagTrim] == NSOrderedSame)
+            {
+                found = YES;
+                break;
+            }
+        }
+        if( !found )
+            [self.tagsView addTag:tagTrim];
+        
         self.tfTags.text = @"";
         self.btnTags.hidden = YES;
-        nbTags++;
     }
 }
 
 -(void) removeTag:(AMTagView *) tag
 {
     [self.tagsView removeTag:tag];
-    nbTags--;
-    if( nbTags == 0 )
+    if( self.tagsView.tags.count == 0 )
         self.btnTags.hidden = NO;
 }
 
@@ -266,12 +307,112 @@ static const int MAX_CARACTERS_AUTHORIZED   = 8;
 
 - (IBAction)actTags:(id)sender
 {
+    activeField = self.btnTags;
+    
+    [UIView animateWithDuration:1.0f animations:^{
+        for( UIControl *element in uiElementFirstBlock )
+        {
+            if( ![element isEqual:self.tagsView] )
+                [element setAlpha:0];
+        }
+    } completion:^(BOOL finished)
+     {
+         self.tfTags.hidden = NO;
+     }];
+    
     [self.tfTags setText:@""];
     [self.tfTags becomeFirstResponder];
 }
 
 - (IBAction)actDescription:(id)sender
 {
+    activeField = self.btnTags;
+}
+
+#pragma mark - Gestion Keyboard
+
+// Call this method somewhere in your view controller setup code.
+- (void)registerForKeyboardNotifications
+
+{
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWasShown:)
+                                                 name:UIKeyboardDidShowNotification object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillBeHidden:)
+                                                 name:UIKeyboardWillHideNotification object:nil];
     
 }
+
+// Called when the UIKeyboardDidShowNotification is sent.
+- (void)keyboardWasShown:(NSNotification*)aNotification
+{
+    keyboardActivated = YES;
+    NSDictionary* info = [aNotification userInfo];
+    CGSize kbSize = [[info objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
+    
+    UIEdgeInsets contentInsets = UIEdgeInsetsMake(0.0, 0.0, kbSize.height, 0.0);
+    self.scrollView.contentInset = contentInsets;
+    self.scrollView.scrollIndicatorInsets = contentInsets;
+ 
+    // If active text field is hidden by keyboard, scroll it so it's visible
+    // Your app might not need or want this behavior.
+    CGRect aRect = self.view.frame;
+    aRect.size.height -= kbSize.height;
+    CGRect convertedRect = [activeField.superview convertRect:activeField.frame toView:self.scrollView];
+    convertedRect.origin.y += 8;
+    if (!CGRectContainsPoint(aRect, convertedRect.origin) ) {
+        savedContentOffset = self.scrollView.contentOffset;
+        [self.scrollView scrollRectToVisible:convertedRect animated:YES];
+    }
+}
+
+// Called when the UIKeyboardWillHideNotification is sent
+- (void)keyboardWillBeHidden:(NSNotification*)aNotification
+{
+    keyboardActivated = NO;
+    UIEdgeInsets contentInsets = UIEdgeInsetsZero;
+    self.scrollView.contentInset = contentInsets;
+    self.scrollView.scrollIndicatorInsets = contentInsets;
+    [self.scrollView setContentOffset:savedContentOffset animated:YES];
+    
+    [UIView animateWithDuration:1.0f animations:^{
+        self.tfTags.hidden = YES;
+        for( UIControl *element in uiElementFirstBlock )
+            [element setAlpha:1];
+    }];
+}
+
+#pragma mark - UITextView
+-(void)textViewDidBeginEditing:(UITextView *)textView
+{
+    [UIView animateWithDuration:1.0f animations:^{
+        for( id element in uiElementFirstBlock )
+        {
+            if( [element isEqual:self.vLocation] )
+            {
+                [element setAlpha:0.5];
+                continue;
+            }
+            if( ![element isEqual:textView] )
+                [element setAlpha:0];
+        }
+    } completion:^(BOOL finished)
+     {
+         [self.view layoutIfNeeded];
+     }];
+}
+
+-(void)textViewDidEndEditing:(UITextView *)textView
+{
+    textView.text = [textView.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+}
+
+- (IBAction)actTapOut:(id)sender
+{
+    [activeField resignFirstResponder];
+    activeField = nil;
+}
+
 @end
