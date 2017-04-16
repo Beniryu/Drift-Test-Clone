0a4f849a0913757febc2ab67ea1e24c44e05774e
Thierry Ng <ng.thierry.d@gmail.com>
2017-04-16 16:17:55 +0200

    Adding VC + adding Lottie pod to repo

---
+++ Drift/DFTDropSignalViewController.h
@@ -0,0 +1,13 @@
+//
+//  DFTDropSignalViewController.h
+//  Drift
+//
+//  Created by Thierry Ng on 16/04/2017.
+//  Copyright © 2017 Thierry Ng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DFTDropSignalViewController : UIViewController
+
+@end
---
+++ Drift/DFTDropSignalViewController.m
@@ -0,0 +1,24 @@
+//
+//  DFTDropSignalViewController.m
+//  Drift
+//
+//  Created by Thierry Ng on 16/04/2017.
+//  Copyright © 2017 Thierry Ng. All rights reserved.
+//
+
+#import "DFTDropSignalViewController.h"
+
+@interface DFTDropSignalViewController ()
+
+@end
+
+@implementation DFTDropSignalViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+	
+}
+
+@end
