d4c47575d13e61cbc79658c453dcb0f90ecf6175
Thierry Ng <ng.thierry.d@gmail.com>
2017-04-17 20:01:21 +0200

    attempting to fix missing reference to SignalViewController

--- Drift/DFTDropSignalViewController.m
+++ Drift/DFTDropSignalViewController.m
@@ -28,6 +28,7 @@
 	return (0);
 }
 
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
 	UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
