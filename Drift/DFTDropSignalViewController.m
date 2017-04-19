ad9c794afa0934e83faafd8a5da3afabde4fe243
Thierry Ng <ng.thierry.d@gmail.com>
2017-04-20 00:36:33 +0200

    All screens layout done

--- Drift/DFTDropSignalViewController.m
+++ Drift/DFTDropSignalViewController.m
@@ -8,10 +8,16 @@
 
 #import "DFTDropSignalViewController.h"
 
-@interface DFTDropSignalViewController () <UITableViewDelegate, UITableViewDataSource>
+@interface DFTDropSignalViewController () <UITableViewDelegate, UITableViewDataSource, UICollectionViewDelegate, UICollectionViewDataSource>
 
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *segmentedControl;
+
+#pragma mark screens
+@property (weak, nonatomic) IBOutlet UICollectionView *recentCollectionView;
+@property (weak, nonatomic) IBOutlet UICollectionView *groupCollectionView;
+@property (weak, nonatomic) IBOutlet UICollectionView *nearbyCollectionView;
+@property (weak, nonatomic) IBOutlet UITableView *allTableView;
 
 @end
 
@@ -22,10 +28,14 @@
 	[super viewDidLoad];
 
 	[self configureScrollView];
+	[self configureRecentScreen];
+	[self configureGroupScreen];
+	[self configureNearbyScreen];
+	[self configureAllScreen];
 }
 
 #pragma mark
-#pragma mark - UITableView
+#pragma mark - Config
 
 - (void)configureScrollView
 {
@@ -35,12 +45,66 @@
 	self.scrollView.showsHorizontalScrollIndicator = NO;
 }
 
+- (void)configureRecentScreen
+{
+	self.recentCollectionView.delegate = self;
+	self.recentCollectionView.dataSource = self;
+}
+
+- (void)configureGroupScreen
+{
+	self.groupCollectionView.delegate = self;
+	self.groupCollectionView.dataSource = self;
+}
+
+- (void)configureNearbyScreen
+{
+	self.nearbyCollectionView.delegate = self;
+	self.nearbyCollectionView.dataSource = self;
+}
+
+- (void)configureAllScreen
+{
+	self.allTableView.delegate = self;
+	self.allTableView.dataSource = self;
+}
+
+#pragma mark
+#pragma mark - UICollectionView
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+	return (1);
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+	if (collectionView == self.recentCollectionView)
+	{
+		UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"Cell" forIndexPath:indexPath];
+
+		return (cell);
+	}
+	else if (collectionView == self.groupCollectionView)
+	{
+		UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"Cell" forIndexPath:indexPath];
+
+		return (cell);
+	}
+	else // self.nearbyCollectionView
+	{
+		UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"Cell" forIndexPath:indexPath];
+
+		return (cell);
+	}
+}
+
 #pragma mark
 #pragma mark - UITableView
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-	return (0);
+	return (1);
 }
 
 
